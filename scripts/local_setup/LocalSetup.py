d478da3fca0c04fef05a990fe53e96454bbdb2a4
minhnhatnoe <86871862+minhnhatnoe@users.noreply.github.com>
2022-11-24 00:25:24 +0700

    Adds flushing to everything

--- scripts/local_setup/LocalSetup.py
+++ scripts/local_setup/LocalSetup.py
@@ -1,7 +1,6 @@
 """Deploy the all parts of the project with a single click."""
 VERSION = 'v1.0.0'
 print(f"Local setup script version {VERSION}", flush=True)
-print("This is experimental")
 
 import time, sys
 import NoIP, Checks, Docker
@@ -13,25 +12,25 @@ if __name__ == '__main__':
     print("Checking config files", flush=True)
     Checks.check_config()
     
-    print("Installing docker")
+    print("Installing docker", flush=True)
     Docker.install()
 
-    print("Calling docker compose up")
+    print("Calling docker compose up", flush=True)
     Docker.compose_up()
 
-    print("Waiting for services to finish setting up")
+    print("Waiting for services to finish setting up", flush=True)
     time.sleep(10)
 
-    print("Checking availability")
+    print("Checking availability", flush=True)
     Checks.check_availability("localhost", False)
 
-    print('Updating No-IP record')
+    print("Updating No-IP record", flush=True)
     no_ip_r = NoIP.no_ip_update(no_ip_domain, no_ip_cred, "abc@def.com", VERSION)
 
-    print('No-IP update success. Response from No-IP: ', no_ip_r)
+    print(f"No-IP update success. Response from No-IP: {no_ip_r}", flush=True)
 
-    print("Waiting 5 mins for No-IP records update")
+    print("Waiting 5 mins for No-IP records update", flush=True)
     time.sleep(5*60)
 
-    print("Checking availability at the real domain")
+    print("Checking availability at the real domain", flush=True)
     Checks.check_availability(real_domain, True)
