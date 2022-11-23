85c8c7536fc7aa9f8898eaacd5d9150a270ace2b
root <86871862+minhnhatnoe@users.noreply.github.com>
2022-11-23 14:44:24 +0000

    Add usage to readme

--- scripts/SingleClick.py
+++ scripts/SingleClick.py
@@ -1,14 +1,16 @@
 import fabric
 from fabric import transfer
 
+
 config_files = ['proxy.env', 'backend.env', 'cert.pem']
 if __name__ == '__main__':
-    print("Entering remote setup mode")
+    print("Setting up remotely")
     ssh_addr = input("Enter SSH host: ")
     real_domain = input("Real domain name: ")
     no_ip_domain = input("No-IP domain name: ")
     no_ip_username = input("No-IP username: ")
     no_ip_password = input("No-IP password: ")
+
     c = fabric.Connection(host=ssh_addr, user="root")
     c.run("sudo apt update; sudo apt -qq -y install git")
     c.run("rm -f -r Deployment")
@@ -16,5 +18,5 @@ if __name__ == '__main__':
 
     t = transfer.Transfer(c)
     for name in config_files:
-        t.put(name, f"Deployment/data/{name}")
+        t.put(f"data/{name}", f"Deployment/data/{name}")
     c.run(f'cd Deployment/scripts; python3 LocalSetup.py {real_domain} {no_ip_domain} {no_ip_username} {no_ip_password}')
