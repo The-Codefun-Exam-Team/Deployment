80b90c6044b09dd7bfbab554cd740f2d5d815553
Nguyen Minh Nhat Noe <86871862+minhnhatnoe@users.noreply.github.com>
2024-04-22 23:12:57 +0700

    feat: chain commands in update.sh

--- update.sh
+++ update.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
-cd ~/Deployment; git pull; docker compose pull; docker compose up -d
+cd ~/Deployment && git pull && docker compose pull && docker compose up -d
 
 # Copy this to ~/update.sh
