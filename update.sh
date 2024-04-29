745e35195b2fb13138f66ff02ddd883d96b9df01
Nguyen Minh Nhat Noe <86871862+minhnhatnoe@users.noreply.github.com>
2024-04-29 18:03:56 +0700

    feat: update.sh error if script not successful

--- update.sh
+++ update.sh
@@ -1,4 +1,9 @@
 #!/bin/sh
-cd ~/Deployment && git pull && docker compose pull && docker compose up -d
+set -e
+
+cd ~/Deployment
+git pull
+docker compose pull
+docker compose up -d
 
 # Copy this to ~/update.sh
